9c92b943d971aa8ed5ad3c66fcce04786d0a01af
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-03-27 23:44:52 +0100

    Working build Transforms based on XSLT Uses LINQ to select XML nodes

--- XSLTExercise1/XSLTExercise1/KerbalNames.xslt
+++ XSLTExercise1/XSLTExercise1/KerbalNames.xslt
@@ -2,11 +2,19 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
 >
-    <xsl:output method="xml" indent="yes"/>
+  <xsl:output method="xml" indent="yes"/>
 
-    <xsl:template match="@* | node()">
-        <xsl:copy>
-            <xsl:apply-templates select="@* | node()"/>
-        </xsl:copy>
-    </xsl:template>
+  <xsl:template match="@* | node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@* | node()"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="@name[starts-with(., 'Bill')]">
+    <xsl:copy/>
+    <xsl:attribute name="isBill">
+      <xsl:value-of select="'true'"/>
+    </xsl:attribute>
+  </xsl:template>
+
 </xsl:stylesheet>
---
+++ XSLTExercise1/XSLTExercise1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.Xml.Linq;
+
+namespace XSLTExercise1
+{
+    class Program
+    {
+        /// <summary>
+        /// Because VS 2017 community edition doesn't have built in
+        /// XSLT debugging or transform view features, this is a C#
+        /// implementation to allow XSLTs to be tested.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing XSLT (transformation)");
+
+            // Locate target XML file, XSLT to transform and an output XML file
+            string relativeFileLocation = "KerbalCadetList.xml";
+            string xsltFileLocation = "KerbalNames.xslt";
+            string transformedXmlOutputFileLocation = relativeFileLocation.Replace(".xml", "") + "-transformed.xml";
+
+
+            TransformXmlFile(relativeFileLocation, xsltFileLocation, transformedXmlOutputFileLocation);
+
+
+            UseLinqToQueryTransformedXml(transformedXmlOutputFileLocation);
+
+
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void TransformXmlFile(string relativeFileLocation, string xsltFileLocation, string transformedXmlOutputFileLocation)
+        {
+            XPathDocument kspXPathDoc = new XPathDocument(relativeFileLocation);
+            XslCompiledTransform xslTrans = new XslCompiledTransform(true);
+            xslTrans.Load(xsltFileLocation);
+            using (XmlTextWriter xmlTransformedWriter = new XmlTextWriter(transformedXmlOutputFileLocation, null))
+            {
+                xslTrans.Transform(kspXPathDoc, null, xmlTransformedWriter);
+            }
+        }
+
+        private static void UseLinqToQueryTransformedXml(string transformedXmlOutputFileLocation)
+        {
+            XmlDocument transformedXmlFromFile = new XmlDocument();
+            transformedXmlFromFile.Load(transformedXmlOutputFileLocation);
+
+            // Select using LINQ
+            XElement readTransformedXml = XElement.Parse(transformedXmlFromFile.OuterXml);
+            var query = from ele in readTransformedXml.Elements("{urn:ksp}kerbal-cadet")
+                        select ele;
+
+            OutputLinqQuerySelect(query);
+
+            query = from ele in readTransformedXml.Elements("{urn:ksp}kerbal-cadet")
+                    select ele;
+
+            OutputLinqQuerySelect(query);
+
+            //TODO: Where clauses to filter XML data by additional attributes and parent node attributes
+        }
+
+        private static void OutputLinqQuerySelect(IEnumerable<XElement> query)
+        {
+            Console.WriteLine("--query output--");
+            foreach (var ele in query)
+                Console.WriteLine(ele);
+        }
+    }
+}
