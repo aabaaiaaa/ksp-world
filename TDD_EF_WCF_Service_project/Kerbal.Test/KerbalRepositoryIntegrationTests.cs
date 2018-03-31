ce3a4647d647e1bed0150780062ffede37d271c3
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-03-31 13:16:03 +0100

    Integration test clean up routine added
    Future integration tests can be scripted to cover very specific areas if needed.

--- TDD_EF_WCF_Service_project/Kerbal.Test/KerbalRepositoryIntegrationTests.cs
+++ TDD_EF_WCF_Service_project/Kerbal.Test/KerbalRepositoryIntegrationTests.cs
@@ -10,6 +10,7 @@ namespace Kerbal.Test
     [TestClass]
     public class KerbalRepositoryIntegrationTests
     {
+        #region Setup
         /// <summary>
         /// Sets up connection string details and EF DbContext for tests
         /// </summary>
@@ -23,6 +24,19 @@ namespace Kerbal.Test
 
         private static Data.KerbalRepository _service;
 
+        /// <summary>
+        /// Remove any existing kerbals
+        /// </summary>
+        [TestInitialize]
+        public void RemoveExistingKerbals()
+        {
+            var existingKerbals = _service.Get().ToList();
+            foreach (var k in existingKerbals)
+                _service.Remove(k);
+        }
+        #endregion
+
+        #region Integration Tests
         [TestMethod]
         public void list_kerbals()
         {
@@ -31,5 +45,30 @@ namespace Kerbal.Test
 
             Assert.AreEqual(0, kerbals.Count());
         }
+
+        [TestMethod]
+        public void add_a_kerbal()
+        {
+            var kerbal = new Data.Kerbal() { Name = "Jebadiah" };
+            _service.Add(kerbal);
+
+            var newlyAddedKerbal = _service.Get("Jebadiah");
+
+            Assert.AreEqual(kerbal, newlyAddedKerbal);
+        }
+
+        [TestMethod]
+        public void list_kerbals_on_missions()
+        {
+            var kerbalOnMission = new Data.Kerbal() { Name = "Gene", OnMission = true };
+            var kerbalNotOnMission = new Data.Kerbal() { Name = "Valentina" };
+            _service.Add(kerbalOnMission);
+            _service.Add(kerbalNotOnMission);
+            var kerbalsOnMissions =_service.Get(true);
+
+            Assert.AreEqual(1, kerbalsOnMissions.Count());
+            Assert.AreEqual("Gene", kerbalsOnMissions.First().Name);
+        }
+        #endregion
     }
 }
