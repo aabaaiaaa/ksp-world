c3adff6e351aa7b870bccc1cadc8a795af424914
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-04-07 14:35:26 +0100

    Extension methods and reflection

---
+++ FundamentalsConsoleApp/FundamentalsConsoleApp/CarExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsConsoleApp
+{
+    public static class CarExtensions
+    {
+        public static bool IsAnExtensionMethod(this FastCarPerson person)
+        {
+            return true;
+        }
+
+        public static void ChangeWheel<T>(this FastCar<T> car) where T : struct
+        {
+            Console.WriteLine($"Changed the wheels for the car: {car.NumberOfGears}");
+        }
+    }
+}
--- FundamentalsConsoleApp/FundamentalsConsoleApp/Program.cs
+++ FundamentalsConsoleApp/FundamentalsConsoleApp/Program.cs
@@ -10,14 +10,26 @@ namespace FundamentalsConsoleApp
     {
         static void Main(string[] args)
         {
+            var personOne = new FastCarPerson() { Name = "Gene" };
+            Console.WriteLine($"IsAnExtensionMethod: {personOne.IsAnExtensionMethod()}");
+
             List<ICar<FastCarPerson>> cars = new List<ICar<FastCarPerson>>();
             cars.Add(new FastCar<FastCarPerson>());
             Console.WriteLine(string.Format("Number of Gears: {0}", cars.First().NumberOfGears));
             Console.ReadKey();
             cars.First().StartEngine();
+            FastCar<FastCarPerson> fastCar = (FastCar<FastCarPerson>)cars.First();
+            fastCar.ChangeWheel();
             cars.First()[1] = new FastCarPerson() { Name = "Gene" };
             Console.ReadKey();
             Console.WriteLine($"Passenger 1: {cars.First()[1].Name}");
+            Console.ReadKey();
+
+            foreach (var prop in fastCar.GetType().GetProperties())
+            {
+                Console.WriteLine($"Property: {prop.Name}");
+            }
+
             Console.ReadKey();
         }
     }
